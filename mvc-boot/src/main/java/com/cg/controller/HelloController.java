aad0e4b2a991c2516e5a949dc3c416a28b7efedc
tanmpath <tanmpath@PUNHWD8QWD0.corp.capgemini.com>
2019-07-25 12:01:37 +0530

    First Commit

--- mvc-boot/src/main/java/com/cg/controller/HelloController.java
+++ mvc-boot/src/main/java/com/cg/controller/HelloController.java
@@ -1,12 +1,28 @@
 package com.cg.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.LinkedList;
+import java.util.List;
 
-@Controller
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cg.entities.*;
+
+
+@RestController
 public class HelloController {
 	@GetMapping("/")  // Home page
 	public String hello() {
 		return "Hello World";
+	}
+	
+	
+	@GetMapping("/products")
+	public List<Product> getProducts(){
+		List<Product> list = new LinkedList<Product>();
+		list.add(new Product(101,"Windows", "64 bit os for desktop and laptops",8000D));
+		list.add(new Product(102,"Ubuntu", "18.04 bit os for desktop and laptops",0D));
+		list.add(new Product(103,"Linux", "64 bit os for desktop and laptops",0D));
+		return list;
 	}
 }
