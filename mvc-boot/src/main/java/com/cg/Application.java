7cfac5887de5e729dd1e0891b1388b04cee67a99
tanmpath <tanmpath@PUNHWD8QWD0.corp.capgemini.com>
2019-07-25 10:41:28 +0530

    Second Commit

--- mvc-boot/src/main/java/com/cg/Application.java
+++ mvc-boot/src/main/java/com/cg/Application.java
@@ -5,8 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+	/**
+	 * This Single line does following 
+	 * 1.Create AnnotationConfigApplicationContext
+	 * 2.It uses @ComponentScan for all child packages 
+	 *   2.1 It picks up HelloController class 
+	 *   2.2 Does Request mapping to '/' 
+	 * 3. Reads application.properties 
+	 *   3.1 Found one property 'server.port = 3000' 
+	 * 4.Launches Embedded Tomcat server and deploy HelloController 
+	 * 5. Wait form you to terminate the process
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 }
- 
